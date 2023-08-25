d3cbdb28b082c38d613d3727ac839e447f5c42cc
Thuong Nguyen <thuongnh.uit@gmail.com>
2023-08-25 09:17:01 +0700

    feat: generic gen

--- cmd/create.go
+++ cmd/create.go
@@ -4,13 +4,19 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"text/template"
 
 	"github.com/spf13/cobra"
+	"github.com/zelic91/zen/common"
 	"github.com/zelic91/zen/config"
 	"gopkg.in/yaml.v3"
 )
+
+const rootTemplatePath = "templates"
 
 // createCmd represents the create command
 var createCmd = &cobra.Command{
@@ -31,7 +37,36 @@ func init() {
 	createCmd.Flags().StringP("to", "t", "testgen", "Destination for generated files")
 }
 
-func create(configFile string, to string) {
+func create(configFile string, outputDir string) {
+	// config := readConfig(configFile)
+
+	// Make the target path
+	if err := makeTargetPath(outputDir); err != nil {
+		log.Fatal(err)
+	}
+
+	// Generate root files
+	generateGeneric(outputDir, fmt.Sprintf("%s/%s", rootTemplatePath, "root"), "")
+
+	// Generate config
+
+	// Generate commands
+
+	// Generate database
+
+	// Generate services
+}
+
+func makeTargetPath(outputDir string) error {
+	dirName := filepath.Dir(outputDir)
+	err := os.MkdirAll(dirName, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func readConfig(configFile string) *config.Config {
 	yamlFile, err := os.ReadFile(configFile)
 	if err != nil {
 		log.Fatal(err)
@@ -44,5 +79,68 @@ func create(configFile string, to string) {
 		log.Fatal(err)
 	}
 
-	log.Printf("%#v\n", config)
+	return &config
+}
+
+func generateGeneric(outputPath string, inputPath string, overridePath string) {
+	var err error
+	templateMap := map[string]string{}
+	fileList := []string{}
+	stack := common.NewStack()
+
+	stack.Push("")
+
+	for {
+		if stack.Len() == 0 {
+			break
+		}
+
+		dirName := stack.Pop().(string)
+		dirs, err := RootFs.ReadDir(fmt.Sprintf("%s%s", inputPath, dirName))
+		if err != nil {
+			fmt.Printf("Error parsing templates: %v\n", err)
+		}
+
+		for _, dir := range dirs {
+			newDirName := fmt.Sprintf("%s/%s", dirName, dir.Name())
+			if dir.IsDir() {
+				stack.Push(newDirName)
+			} else {
+				templateMap[dir.Name()] = newDirName
+				fileList = append(fileList, fmt.Sprintf("%s%s", inputPath, newDirName))
+			}
+		}
+	}
+
+	templates, err = template.ParseFS(
+		RootFs,
+		fileList...,
+	)
+	if err != nil {
+		fmt.Printf("Error parsing templates: %v\n", err)
+	}
+
+	for _, tmpl := range templates.Templates() {
+		outFileName := fmt.Sprintf("%s/%s", outputPath, templateMap[tmpl.Name()])
+
+		var filePath *os.File
+		dirName := filepath.Dir(outFileName)
+		err := os.MkdirAll(dirName, os.ModePerm)
+		if err != nil {
+			fmt.Printf("error creating parent dirs %v", err)
+			return
+		}
+
+		filePath, err = os.Create(outFileName)
+		if err != nil {
+			fmt.Printf("error creating output file %v", err)
+			return
+		}
+
+		defer filePath.Close()
+
+		if err := templates.ExecuteTemplate(filePath, tmpl.Name(), data); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
